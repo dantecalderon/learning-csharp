4c5e1e0278f4f8f8f16b1843ce890a98dd670656
Dante Calderon <mail@dantecalderon.dev>
2020-01-12 15:05:49 -0500

    Create class example

---
+++ school/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace school
+{
+    class School {
+        public string name;
+        public string address;
+        public int foundationYear;
+
+        public void Beep()
+        {
+            Console.Beep();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mySchool = new School();
+
+            mySchool.name = "Umbrella Academy";
+            mySchool.address = "4 King St E in Hamilton";
+            mySchool.foundationYear = 1678;
+
+            Console.WriteLine("BEEP");
+            mySchool.Beep();
+        }
+    }
+}
