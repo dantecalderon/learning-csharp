3af7465394cbc8891618fd2b871b4933ef132faf
Dante Calderon <mail@dantecalderon.dev>
2020-01-12 19:19:11 -0500

    Create array and iterate

--- CoreSchool/Program.cs
+++ CoreSchool/Program.cs
@@ -8,24 +8,32 @@ namespace CoreSchool
         static void Main(string[] args)
         {
             var mySchool = new CoreSchool.Entities.School("The Umbrella Academy", 1678, SchoolType.PreSchool);
-            // mySchool.Country = "USA";
-            // mySchool.Type = SchoolType.PreSchool;
 
-            var course1 = new Course() {
+            var courses = new Course[3];
+
+            courses[0] = new Course() {
                 Name = "101"
             };
-            var course2 = new Course() {
+
+            courses[1] = new Course() {
                 Name = "256"
             };
-            var course3 = new Course() {
+
+            courses[2] = new Course {
                 Name = "512"
             };
 
             Console.WriteLine(mySchool);
             Console.WriteLine(new String('=', 40));
-            Console.WriteLine($"{course1.Name}, {course1.UniqueId}");
-            Console.WriteLine($"{course2.Name}, {course2.UniqueId}");
-            Console.WriteLine($"{course3.Name}, {course3.UniqueId}");
+            PrintCourses(courses);
+        }
+
+        private static void PrintCourses(Course[] courses)
+        {   
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"{course.Name}, {course.UniqueId}");
+            }
         }
     }
 }
